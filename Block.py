959e91e0cde5bf7ece8c41b47a40cd32192a090f
The Exodus <148576738+theeexodus@users.noreply.github.com>
2023-10-29 13:59:03 +0530

    Add files via upload

---
+++ Block.py
@@ -0,0 +1,16 @@
+from hashlib import sha256
+class Block:
+    def __init__(self, index, transactions, prev_hash):
+        self.index = index  
+        self.transactions = transactions 
+        self.prev_hash = prev_hash 
+        self.nonce = 0 
+
+
+   
+    def generate_hash(self):
+        all_data_combined = str(self.index) + str(self.nonce) + self.prev_hash + str(self.transactions)
+        return sha256(all_data_combined.encode()).hexdigest()
+    
+    def add_t(self, t):
+        self.transactions.append(t)
---
+++ installguide.md
@@ -0,0 +1,6 @@
+1. Install required libraries using :
+   `pip install -r requirements.txt`
+2. Open one terminal and start server/peer:
+   `python peer.py`
+3. Open another terminal and start a client:
+   `python run_app.py`
